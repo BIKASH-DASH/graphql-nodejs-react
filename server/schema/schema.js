ed18092ebc2db9691b3896a53692d94c4ae1513f
bikash-dash <vikashdash004@gmail.com>
2021-06-16 14:35:18 +0530

    GraphQLObjectType

---
+++ server/schema/schema.js
@@ -0,0 +1,14 @@
+const {graphql,GraphQLString} = require('graphql')
+
+
+const {GraphQLObjectType} = graphql ;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields:()=>({
+        id:{type:GraphQLString},
+        name: {type:GraphQLString},
+        genre:{type:GraphQLString}
+    })
+});
+
